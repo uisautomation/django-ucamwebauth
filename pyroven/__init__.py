a9647b6e0e33106f6b08f05403364d416ac8b2ae
Kristian Glass <git@doismellburning.co.uk>
2012-06-29 19:53:09 +0100

    Removed LGPL notices now we're all MIT-ed up...

--- pyroven/__init__.py
+++ pyroven/__init__.py
@@ -1,20 +1,4 @@
 #   Copyright 2011 Andrew Ryrie (amr66)
-#
-#   This file is part of pyroven
-#
-#   pyroven is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Lesser General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-
-#   pyroven is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-
-#   You should have received a copy of the GNU Lesser General Public License
-#   along with pyroven.  If not, see <http://www.gnu.org/licenses/>.
-
 
 """Python-based authentication for the University of Cambridge's UCam-WebAuth
 service, aka Raven."""
