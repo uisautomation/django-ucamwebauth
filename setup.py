89313c52cca5e257223566f075a4b3c92b7885a6
Kristian Glass <git@doismellburning.co.uk>
2012-07-01 01:40:36 +0100

    No long_description (or at least not of README.md)
    http://bugs.python.org/issue13614 - setup.py register fails if
    long_description contains ReST :(

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from distutils.core import setup
 
 setup(name='pyroven',
       description='A Django authentication backend for Ucam-WebAuth / Raven',
-      long_description=open('README.md').read(),
+#     No long_description due to http://bugs.python.org/issue13614
+#     long_description=open('README.md').read(),
       url='https://github.com/pyroven/django-pyroven',
       version='0.9',
       license='MIT',
