e855d65ed45592d6da9621ebf295a3b0cc4e0310
Kristian Glass <git@doismellburning.co.uk>
2012-07-01 01:41:17 +0100

    django-pyroven is the name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from distutils.core import setup
 
-setup(name='pyroven',
+setup(name='django-pyroven',
       description='A Django authentication backend for Ucam-WebAuth / Raven',
 #     No long_description due to http://bugs.python.org/issue13614
 #     long_description=open('README.md').read(),
