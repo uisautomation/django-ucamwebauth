577969573c4274201c23f74b171b822f835cbf2f
JV Clemence <james@jvc26.org>
2012-09-11 12:14:27 +0100

    Added configuration for testing: TravisCI config, requirements.txt, runtests.py script for calling DjangoTestSuiteRunner based on SO answer http://stackoverflow.com/a/12260597/781909

---
+++ runtests.py
@@ -0,0 +1,63 @@
+import os, sys
+
+from django.conf import settings
+
+DIRNAME = os.path.dirname(__file__)
+
+settings.configure(
+    DEBUG=True,
+    DATABASES = {'default': {
+                    'ENGINE': 'django.db.backends.sqlite3', 
+                    'NAME': 'test.db',                      
+                    }
+                },
+    TIME_ZONE = 'Europe/London',
+    USE_TZ = True,
+    ROOT_URLCONF='pyroven.urls',
+    INSTALLED_APPS = ('django.contrib.auth',
+                      'django.contrib.contenttypes',
+                      'django.contrib.sessions',
+                      'django.contrib.sites',
+                      'django.contrib.messages',
+                      'django.contrib.staticfiles',
+                      'pyroven',
+                    ),
+    AUTHENTICATION_BACKENDS = (
+                            'pyroven.backends.RavenAuthBackend',
+                            'django.contrib.auth.backends.ModelBackend'
+                            ),
+    PYROVEN_LOGIN_URL = 'https://demo.raven.cam.ac.uk/auth/authenticate.html',
+    PYROVEN_LOGOUT_URL = 'https://demo.raven.cam.ac.uk/auth/logout.html',
+    PYROVEN_RETURN_URL = 'http://www.example.org/raven_return/',
+    PYROVEN_CERTS = {'901': """-----BEGIN CERTIFICATE-----
+MIIDzTCCAzagAwIBAgIBADANBgkqhkiG9w0BAQQFADCBpjELMAkGA1UEBhMCR0Ix
+EDAOBgNVBAgTB0VuZ2xhbmQxEjAQBgNVBAcTCUNhbWJyaWRnZTEgMB4GA1UEChMX
+VW5pdmVyc2l0eSBvZiBDYW1icmlkZ2UxLTArBgNVBAsTJENvbXB1dGluZyBTZXJ2
+aWNlIERFTU8gUmF2ZW4gU2VydmljZTEgMB4GA1UEAxMXUmF2ZW4gREVNTyBwdWJs
+aWMga2V5IDEwHhcNMDUwNzI2MTMyMTIwWhcNMDUwODI1MTMyMTIwWjCBpjELMAkG
+A1UEBhMCR0IxEDAOBgNVBAgTB0VuZ2xhbmQxEjAQBgNVBAcTCUNhbWJyaWRnZTEg
+MB4GA1UEChMXVW5pdmVyc2l0eSBvZiBDYW1icmlkZ2UxLTArBgNVBAsTJENvbXB1
+dGluZyBTZXJ2aWNlIERFTU8gUmF2ZW4gU2VydmljZTEgMB4GA1UEAxMXUmF2ZW4g
+REVNTyBwdWJsaWMga2V5IDEwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALhF
+i9tIZvjYQQRfOzP3cy5ujR91ZntQnQehldByHlchHRmXwA1ot/e1WlHPgIjYkFRW
+lSNcSDM5r7BkFu69zM66IHcF80NIopBp+3FYqi5uglEDlpzFrd+vYllzw7lBzUnp
+CrwTxyO5JBaWnFMZrQkSdspXv89VQUO4V4QjXV7/AgMBAAGjggEHMIIBAzAdBgNV
+HQ4EFgQUgjC6WtA4jFf54kxlidhFi8w+0HkwgdMGA1UdIwSByzCByIAUgjC6WtA4
+jFf54kxlidhFi8w+0HmhgaykgakwgaYxCzAJBgNVBAYTAkdCMRAwDgYDVQQIEwdF
+bmdsYW5kMRIwEAYDVQQHEwlDYW1icmlkZ2UxIDAeBgNVBAoTF1VuaXZlcnNpdHkg
+b2YgQ2FtYnJpZGdlMS0wKwYDVQQLEyRDb21wdXRpbmcgU2VydmljZSBERU1PIFJh
+dmVuIFNlcnZpY2UxIDAeBgNVBAMTF1JhdmVuIERFTU8gcHVibGljIGtleSAxggEA
+MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEEBQADgYEAsdyB+9szctHHIHE+S2Kg
+LSxbGuFG9yfPFIqaSntlYMxKKB5ba/tIAMzyAOHxdEM5hi1DXRsOok3ElWjOw9oN
+6Psvk/hLUN+YfC1saaUs3oh+OTfD7I4gRTbXPgsd6JgJQ0TQtuGygJdaht9cRBHW
+wOq24EIbX5LquL9w+uvnfXw=
+-----END CERTIFICATE-----
+"""},
+)
+
+from django.test.simple import DjangoTestSuiteRunner
+
+test_runner = DjangoTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(['pyroven', ])
+if failures:
+    sys.exit(failures)
