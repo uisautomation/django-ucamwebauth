7e31ffe7b5dbf33d396bca786d0d3f146db432d4
Matthew Vernon <mcv21@cam.ac.uk>
2013-12-11 16:17:30 +0000

    Correct url patterns
    the default login/logout urls are accounts/login not just login.
    
    signed-off-by: Matthew Vernon <mcv21@cam.ac.uk>

--- pyroven/urls.py
+++ pyroven/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from pyroven.views import raven_login, raven_logout, raven_return
 
 urlpatterns = patterns('',
-    url(r'^login/$', raven_login, name='raven_login'),
-    url(r'^logout/$', raven_logout, name='raven_logout'),
+    url(r'^accounts/login/$', raven_login, name='raven_login'),
+    url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
     url(r'^raven_return/$', raven_return, name='raven_return'),
 )
