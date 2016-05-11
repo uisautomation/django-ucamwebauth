ac7b64874e0696f8e671e3beff75346f27ce4377
Abraham Martin <amc203@cam.ac.uk>
2016-05-11 10:52:13 +0100

    Add compatibility with django 1.8 and 1.9, solve deprecation issues

--- runtestsjenkins.py
+++ runtestsjenkins.py
@@ -55,6 +55,28 @@ LSxbGuFG9yfPFIqaSntlYMxKKB5ba/tIAMzyAOHxdEM5hi1DXRsOok3ElWjOw9oN
 wOq24EIbX5LquL9w+uvnfXw=
 -----END CERTIFICATE-----
 """},
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [
+                # insert your TEMPLATE_DIRS here
+            ],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                    # list if you haven't customized them:
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ],
     JENKINS_TASKS = (
         'django_jenkins.tasks.run_pylint',
     #    'django_jenkins.tasks.run_csslint',
--- ucamwebauth/urls.py
+++ ucamwebauth/urls.py
@@ -1,9 +1,17 @@
+import django
 from django.conf.urls import patterns, url
 from ucamwebauth.views import raven_login, raven_logout, raven_return
 
-urlpatterns = patterns(
-    '',
-    url(r'^accounts/login/$', raven_login, name='raven_login'),
-    url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
-    url(r'^raven_return/$', raven_return, name='raven_return'),
-)
+if django.VERSION[0] <= 1 and django.VERSION[1] <= 7:
+    urlpatterns = patterns(
+        '',
+        url(r'^accounts/login/$', raven_login, name='raven_login'),
+        url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
+        url(r'^raven_return/$', raven_return, name='raven_return'),
+    )
+else:
+    urlpatterns = [
+        url(r'^accounts/login/$', raven_login, name='raven_login'),
+        url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
+        url(r'^raven_return/$', raven_return, name='raven_return'),
+    ]
