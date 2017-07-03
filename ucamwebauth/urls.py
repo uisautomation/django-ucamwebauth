c3ef0a1cd229d5dc7737c5d6a85532d7dfab5388
Abraham Martin <amc203@cam.ac.uk>
2017-07-03 10:39:17 +0100

    removed unnecessary patch for urls.py for supporting django 1.7

--- ucamwebauth/urls.py
+++ ucamwebauth/urls.py
@@ -1,17 +1,8 @@
-import django
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from ucamwebauth.views import raven_login, raven_logout, raven_return
 
-if django.VERSION[0] <= 1 and django.VERSION[1] <= 7:
-    urlpatterns = patterns(
-        '',
-        url(r'^accounts/login/$', raven_login, name='raven_login'),
-        url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
-        url(r'^raven_return/$', raven_return, name='raven_return'),
-    )
-else:
-    urlpatterns = [
-        url(r'^accounts/login/$', raven_login, name='raven_login'),
-        url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
-        url(r'^raven_return/$', raven_return, name='raven_return'),
-    ]
+urlpatterns = [
+    url(r'^accounts/login/$', raven_login, name='raven_login'),
+    url(r'^accounts/logout/$', raven_logout, name='raven_logout'),
+    url(r'^raven_return/$', raven_return, name='raven_return'),
+]
