eb31c0305b3b5d06cf293616b2ff7243a2f71766
Mike Bamford <mb2174@cam.ac.uk>
2017-08-02 14:33:22 +0100

    Added string representation to UserProfile.

--- ucamwebauth/models.py
+++ ucamwebauth/models.py
@@ -12,3 +12,6 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     raven_for_life = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user)
